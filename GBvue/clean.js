7a6f4600061cd870f58f052972186e22da8ca0f5
Svetoslav <veiv@mail.ru>
2023-08-20 20:29:17 +0300

    20 aug

--- GBvue/clean.js
+++ GBvue/clean.js
@@ -1,5 +1,9 @@
-let str = 'abcde';
-let arr = str.split('');
-arr.reverse()
-
+const arr = [
+    {price: 2},
+    {price: 1},
+    {price: 9},
+    {price: 4}
+]
+arr.sort((a, b) => a.price > b.price ? 1 : -1)
 console.log(arr);
+
